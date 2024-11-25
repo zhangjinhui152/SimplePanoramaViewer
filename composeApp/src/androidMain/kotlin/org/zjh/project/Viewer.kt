5886956b287b7d13d31087f4f57d785290d4a38d
zhangjinhui152 <2233274242@qq.com>
2024-11-25 11:57:16 +0800

    feat 可以选择其他图片

--- composeApp/src/androidMain/kotlin/org/zjh/project/Viewer.kt
+++ composeApp/src/androidMain/kotlin/org/zjh/project/Viewer.kt
@@ -7,6 +7,7 @@ import android.net.Uri
 import android.provider.MediaStore
 import android.util.AttributeSet
 import android.view.MotionEvent
+import android.view.ViewGroup
 import android.widget.RelativeLayout
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
@@ -24,81 +25,98 @@ import androidx.compose.ui.viewinterop.AndroidView
 import com.panoramagl.PLImage
 import com.panoramagl.PLManager
 import com.panoramagl.PLSphericalPanorama
+import com.panoramagl.PLTexture
 import timber.log.Timber
 
 
 class Viewer {
     private var pl: PLManager? = null
-    private var plRow: PLManager? = null
+    lateinit var currentView: CustomRelativeLayout
     private var context: Context? = null
     private fun setPlManagerAndContext(pl: PLManager, context: Context) {
         this.pl = pl
         this.context = context
     }
 
-    var count = 0
+    private var count = 0
 
     private fun getBitmapFromUriAndSetBitmap(uri: Uri): Unit {
         Timber.d("Debug message pl ${this.pl}")
         Timber.d("Debug message context. ${this.context}")
-        if (count >= 1) {
 
-        }
-        try {
-            val panorama = PLSphericalPanorama()
-            val originalBitmap = MediaStore.Images.Media.getBitmap(context?.contentResolver, uri)
-            // 缩放图片
-            // 计算目标尺寸
-            val maxWidth = 8192
-            val maxHeight = 8192
 
-            val scaledBitmap =
-                if (originalBitmap.width > maxWidth || originalBitmap.height > maxHeight) {
-                    val scaleWidth = maxWidth.toFloat() / originalBitmap.width
-                    val scaleHeight = maxHeight.toFloat() / originalBitmap.height
-                    val scaleFactor = if (scaleWidth < scaleHeight) scaleWidth else scaleHeight
-                    Bitmap.createScaledBitmap(
-                        originalBitmap,
-                        (originalBitmap.width * scaleFactor).toInt(),
-                        (originalBitmap.height * scaleFactor).toInt(),
-                        true
-                    )
-                } else {
-                    originalBitmap
-                }
-            Timber.d("Debug message $scaledBitmap");
+        val scaledBitmap = getSbitmap(uri)
+        Timber.d("Debug message $scaledBitmap");
+        if (count != 0) {
+            this.pl = null
+            this.pl = PLManager(context!!)
+            Timber.d("this.pl ${this.pl}")
+            this.pl?.setContentView(currentView)
+            currentView.plManager = this.pl!!
             this.pl!!.onCreate()
-            Timber.d("Debug message $uri");
-            Timber.d("Debug message ${this.pl?.panorama}");
-            Timber.d("Debug message isRendererCreated ${this.pl?.isRendererCreated}");
-            Timber.d("Debug message context ${this.context}");
+            val panorama = PLSphericalPanorama()
             panorama.camera.lookAt(30.0f, 90.0f)
-            panorama.setImage(
-                PLImage(
-                    scaledBitmap,
-                    false
-                )
-            )
+            val plImage = PLImage(scaledBitmap, false)
+            panorama.setImage(plImage)
             this.pl?.panorama = panorama
-            count++
 
-        } catch (e: Exception) {
-            e.printStackTrace()
-            Timber.d("Debug message 炸了");
+        } else {
+            val panorama = PLSphericalPanorama()
+            Timber.d("Debug message 第one次 ${panorama.texturesLength()}");
+            Timber.d("Debug message 第one次");
+            this.pl!!.onCreate()
+            panorama.camera.lookAt(30.0f, 90.0f)
+            val plImage = PLImage(scaledBitmap, false)
+            panorama.setImage(plImage)
+            this.pl?.panorama = panorama
+
         }
+        Timber.d("Debug message $uri");
+        Timber.d("Debug message ${this.pl?.panorama}");
+        Timber.d("Debug message isRendererCreated ${this.pl?.isRendererCreated}");
+        Timber.d("Debug message context ${this.context}");
+        count++
+
+
+    }
+
+    private fun getSbitmap(uri: Uri): Bitmap {
+        val originalBitmap = MediaStore.Images.Media.getBitmap(context?.contentResolver, uri)
+        // 缩放图片
+        // 计算目标尺寸
+        val maxWidth = 8192
+        val maxHeight = 8192
+
+        val scaledBitmap =
+            if (originalBitmap.width > maxWidth || originalBitmap.height > maxHeight) {
+                val scaleWidth = maxWidth.toFloat() / originalBitmap.width
+                val scaleHeight = maxHeight.toFloat() / originalBitmap.height
+                val scaleFactor = if (scaleWidth < scaleHeight) scaleWidth else scaleHeight
+                Bitmap.createScaledBitmap(
+                    originalBitmap,
+                    (originalBitmap.width * scaleFactor).toInt(),
+                    (originalBitmap.height * scaleFactor).toInt(),
+                    true
+                )
+            } else {
+                originalBitmap
+            }
+        return scaledBitmap
     }
 
 
     @SuppressLint("ViewConstructor")
     class CustomRelativeLayout(
-        context: Context,
-        attrs: AttributeSet? = null,
-        private val plManager: PLManager
+        context: Context, attrs: AttributeSet? = null, var plManager: PLManager
     ) : RelativeLayout(context, attrs) {
+
+
         @SuppressLint("ClickableViewAccessibility")
         override fun onTouchEvent(event: MotionEvent): Boolean {
-            return plManager.onTouchEvent(event)
+            Timber.d("Debug ClickableViewAccessibility ${this.plManager}")
+            return this.plManager.onTouchEvent(event)
         }
+
 
 
     }
@@ -112,26 +130,16 @@ class Viewer {
         AndroidView(
             factory = { ctx ->
                 plManager = PLManager(ctx)
+                this.pl = plManager
                 this.setPlManagerAndContext(plManager, ctx)
                 //Here you can construct your View
-                val view = CustomRelativeLayout(ctx, plManager = plManager)
-                Timber.d("Debug message pl ${plManager}")
+                currentView = CustomRelativeLayout(ctx, plManager = this.pl!!)
+
+                Timber.d("Debug message pl ${this.pl}")
                 Timber.d("Debug message context. ${ctx}")
-                plManager.setContentView(view)
-//                plManager.onCreate()
-//
-//                val panorama = PLSphericalPanorama()
-//                panorama.camera.lookAt(30.0f, 90.0f)
-//                panorama.setImage(
-//                    PLImage(
-//                        PLUtils.getBitmap(ctx, R.drawable.as1),
-//                        false
-//                    )
-//                )
-//                plManager.panorama = panorama
-                view
-            },
-            modifier = Modifier.padding(8.dp)
+                this.pl?.setContentView(currentView)
+                currentView
+            }, modifier = Modifier.padding(8.dp)
 
         )
 
